3757a080199b9f8aa123d01c178f7cbf1b22e6b4
Vincent BIRET <vincentbiret@hotmail.com>
2015-05-09 14:14:18 -0400

    Adding forgotten class to test project

---
+++ AlchemyAPIClient.UnitTest/EntitiesTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlchemyAPIClient.Requests;
+using AlchemyAPIClient.Responses;
+
+namespace AlchemyAPIClient.UnitTest
+{
+    [TestClass]
+    public class EntitiesTest
+    {
+        [TestMethod]
+        public void EntitiesFromText()
+        {
+            foreach (var text in DocumentsProvider.Documents.Value)
+            {
+                var request = new AlchemyTextEntitiesRequest(text, AlchemyClientProvider.AlchemyClient.Value)
+                {
+                    Coreference = true,
+                    Disambiguate = true,
+                    KnowledgeGraph = true,
+                    LinkedData = true,
+                    Quotations = true,
+                    Sentiment = true,
+                    ShowSourceText = true,
+                    StructuredEntities = true,
+                    MaxRetrieve = 30
+                };
+                var requestTask = request.GetResponse();
+                var awaiter = requestTask.GetAwaiter();
+                var response = awaiter.GetResult();
+                Assert.AreEqual(response.Status, AlchemyAPIResponseStatus.OK);
+                Assert.IsNotNull(response.Entities);
+            }
+        }
+    }
+}
