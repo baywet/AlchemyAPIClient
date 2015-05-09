ec4c641087db0a030f60da78e0bc2538c884c64e
Vincent BIRET <vincentbiret@hotmail.com>
2015-05-08 22:17:55 -0400

    Forgot a file checking-in relations

---
+++ AlchemyAPIClient/Requests/Relations/AlchemyHtmlRelationsRequestBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlchemyAPIClient.Requests
+{
+    public abstract class AlchemyHtmlRelationsRequestBase : AlchemyRelationsRequestBase
+    {
+        public AlchemyHtmlRelationsRequestBase (AlchemyClient client):base(client)
+        {
+
+        }
+    }
+}
