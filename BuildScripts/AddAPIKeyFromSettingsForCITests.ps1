42b490bcc1c95b6a6776d7a400b00401539789b8
Vincent BIRET <vincentbiret@hotmail.com>
2015-08-14 19:35:42 -0400

    Changing working directory for key replacement

--- BuildScripts/AddAPIKeyFromSettingsForCITests.ps1
+++ BuildScripts/AddAPIKeyFromSettingsForCITests.ps1
@@ -1,6 +1,6 @@
 param([String]$ApiKey)
 
-$configPath = Resolve-Path "..\AlchemyAPIClient.UnitTest\App.config";
+$configPath = Resolve-Path ".\App.config";
 $regexPattern = '<add\s?key="AlchemyEndPointKey"\s?value=".*"\s?/>';
 $targetSetting = '<add key="AlchemyEndPointKey" value="'+$ApiKey+'" />';
 ((get-content $configPath) -replace $regexPattern, $targetSetting) | set-content $configPath -Verbose;
