92f8492171b515a77f0f7c97eb4f5d9947d17752
Vincent BIRET <vincentbiret@hotmail.com>
2015-08-14 20:27:55 -0400

    adding a script to automatically increment build numbers

---
+++ BuildScripts/VersionIncrementer.ps1
@@ -0,0 +1,7 @@
+$regexPattern = '\[assembly:\s+Assembly(?:File)Version\(\"\d{1,4}\.\d{1,4}\.\d{1,4}\.\d{1,4}\"\)\]';
+$assemblyFiles = get-childitem -filter "*AssemblyInfo.cs" -Recurse;
+$targetVersionInfo = '[assembly: AssemblyFileVersion ("'+$Env:BUILD_BUILDNUMBER+'")]';
+foreach($assemblyFile in $assemblyFiles)
+{
+    ((get-content $assemblyFile.FullName) -replace $regexPattern, $targetVersionInfo) | Out-File -FilePath $assemblyFile.FullName;
+}
