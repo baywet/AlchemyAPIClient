31741a38ddf3fb5cc8f650e05a6897ceed14c421
Vincent BIRET <vincentbiret@hotmail.com>
2015-10-08 23:06:06 -0400

    Improving version replacement to handle draft build defs

--- BuildScripts/releaseNotesChanger.ps1
+++ BuildScripts/releaseNotesChanger.ps1
@@ -1,10 +1,17 @@
 param([String]$releaseNotes)
+function getCleanBuildNumber()
+{
+	$regex = [Regex]"(?<buildnumber>\d{1,4}\.\d{1,4}\.\d{1,4}\.\d{1,4})";
+	$match = $regex.Match($Env:BUILD_BUILDNUMBER);
+	return $match.Groups["buildnumber"].Value;
+}
+$buildNumber = getCleanBuildNumber;
 $regexPatternNotes = '<releaseNotes></releaseNotes>';
 $regexPatternVersion = '<version>.*</version>';
 $assemblyFiles = get-childitem -filter "*AlchemyAPIClient.nuspec" -Recurse;
 $targetNotesnInfo = '<releaseNotes>'+$releaseNotes+'</releaseNotes>';
-$targetVersionInfo = '<version>'+$Env:BUILD_BUILDNUMBER+'</version>';
+$targetVersionInfo = '<version>'+$buildNumber+'</version>';
 foreach($assemblyFile in $assemblyFiles)
 {
-    (((get-content $assemblyFile.FullName) -replace $regexPatternNotes, $targetNotesnInfo) -replace $regexPatternVersion, $targetVersionInfo) | Out-File -FilePath $assemblyFile.FullName -Verbose;
+    (((get-content $assemblyFile.FullName) -replace $regexPatternNotes, $targetNotesnInfo) -replace $regexPatternVersion, $targetVersionInfo) | Out-File -FilePath $assemblyFile.FullName -Encoding utf8 -Verbose;
 }
