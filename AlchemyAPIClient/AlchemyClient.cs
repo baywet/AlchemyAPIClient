84f1796536af098a8c700ead992edb9fe283f6c7
Vincent BIRET <vincentbiret@hotmail.com>
2015-10-09 20:24:01 -0400

    Changing default endpoint to https

--- AlchemyAPIClient/AlchemyClient.cs
+++ AlchemyAPIClient/AlchemyClient.cs
@@ -7,7 +7,7 @@ namespace AlchemyAPIClient
         public string EndPointKey { get; private set; }
         public int MaxRetriesWhenTimeOut { get; set; } = 3;
         public int RetryWaitTimeForNetworkErrorsInMS { get; set; } = 100;
-        public AlchemyClient(string endPointKey, string endPointUrl = "http://access.alchemyapi.com/calls/")
+        public AlchemyClient(string endPointKey, string endPointUrl = "https://access.alchemyapi.com/calls/")
         {
             EndPointUrl = endPointUrl;
             EndPointKey = endPointKey;
